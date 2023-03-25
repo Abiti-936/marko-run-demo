4565ed0ce60d596c8328e35784374a7e5a07b31e
Abiti-936 <amanhara28@gmail.com>
2023-03-25 04:27:44 +0000

    Fix Xata Config

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,22 +1,80 @@
-import { xataWorker } from "./xata";
+import { NotesRecord, xataWorker } from "./xata";
+import urlSlug from "url-slug";
+import { marked } from "marked";
+import { z } from "zod";
+import xss from "xss";
 
-export async function getNotes() {
-  const query = xataWorker("getNotes", async ({ xata }) => {
-    return await xata.db.notes.getAll({
+export async function getNotes(uid: string) {
+  const query = xataWorker("getNotes", async ({ xata }, uid: string) => {
+    return await xata.db.notes.filter("author.id", uid).getAll({
       cache: 86400 * 15,
     });
   });
 
-  return await query();
+  return await query(uid);
 }
 
-export async function getNote(slug: string) {
-  const query = xataWorker("getNote", async ({ xata }, slug: string) => {
-    return await xata.db.notes.filter("slug", slug).getFirst({
-      cache: 86400 * 15,
-      columns: ["*", "author.*"],
-    });
+export async function getNote(slug: string, uid: string) {
+  const query = xataWorker(
+    "getNote",
+    async ({ xata }, slug: string, uid: string) => {
+      return await xata.db.notes
+        .filter("author.id", uid)
+        .filter("slug", slug)
+        .getFirst({
+          cache: 86400 * 15,
+          columns: ["*", "author.*"],
+        });
+    }
+  );
+
+  return await query(slug, uid);
+}
+
+export function filterNotes(data: NotesRecord, author: string) {
+  const schema = z.object({
+    title: z.string().min(10),
+    description: z.string().min(10),
+    markdown: z.string(),
   });
 
-  return await query(slug);
+  try {
+    const created_at = new Date().toUTCString();
+    const { title, description, markdown } = schema.parse(data);
+    const slug = urlSlug(title);
+
+    const html = marked(markdown);
+    const body = xss(html, {
+      stripIgnoreTagBody: true,
+      stripBlankChar: true,
+      stripIgnoreTag: true,
+      escapeHtml: (html: string) =>
+        html.replace(/</g, "&lt;").replace(/>/g, "&gt;"),
+    });
+
+    return {
+      title,
+      description,
+      created_at,
+      markdown,
+      body,
+      author,
+      slug,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
+
+export async function createNote(data: NotesRecord) {
+  const query = xataWorker(
+    "createNote",
+    async ({ xata }, data: NotesRecord) => {
+      return await xata.db.notes.create(data);
+    }
+  );
+
+  return await query(data);
 }
